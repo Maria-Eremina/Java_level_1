e859bd214462a78631eea889dc8ca4a14c1ada11
Maria Eremina <eremina.1906@gmail.com>
2020-11-06 18:32:29 +0300

    Домашнее задание : убраны комментарии с методов

--- src/lesson_2/class_04_11.java
+++ src/lesson_2/class_04_11.java
@@ -3,14 +3,16 @@ package lesson_2;
 public class class_04_11 {
     public static void main(String[] args) {
         System.out.println("education_class_1");
+        invertArray();
         int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        //method3(a);
+        method2();
+        method3(a);
         System.out.println("Максимум в массиве: " + arr_max(a) );
         System.out.println("Минимум в массиве: " + arr_min(a) );
 
     }
 
-    /*public static void invertArray() {
+    public static void invertArray() {
         int[] arr = {1, 0, 1, 0, 0, 0, 1, 0, 1};
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] == 0){
@@ -26,7 +28,7 @@ public class class_04_11 {
 
     }
 
-    public static void invertArray1() {
+    public static void method2() {
         int[] b = new int[8];
         int x = 1;
         for (int i = 0; i < b.length; i++) {
@@ -35,8 +37,8 @@ public class class_04_11 {
             System.out.println(b[i]);
         }
     }
-*/
-    /*public static void method3(int[] b) {
+
+    public static void method3(int[] b) {
         for (int i = 0; i < b.length; i++) {
             if (b[i] < 6) {
                 b[i] *= 2;
@@ -44,7 +46,7 @@ public class class_04_11 {
             System.out.println(b[i]);
         }
 
-    }*/
+    }
 
     public static int arr_max(int[] arr) {
         int max = arr[0];
