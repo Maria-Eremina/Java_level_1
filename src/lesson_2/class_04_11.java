a515cffae34ef9b1140b23a0f1315d7d42f12186
Maria Eremina <eremina.1906@gmail.com>
2020-11-06 18:22:49 +0300

    Домашнее задание : методы 4/4

--- src/lesson_2/class_04_11.java
+++ src/lesson_2/class_04_11.java
@@ -3,8 +3,11 @@ package lesson_2;
 public class class_04_11 {
     public static void main(String[] args) {
         System.out.println("education_class_1");
-         int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-         method3(a);
+        int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        //method3(a);
+        System.out.println("Максимум в массиве: " + arr_max(a) );
+        System.out.println("Минимум в массиве: " + arr_min(a) );
+
     }
 
     /*public static void invertArray() {
@@ -33,13 +36,31 @@ public class class_04_11 {
         }
     }
 */
-    public static void method3(int[] b) {
-        for(int i = 0; i < b.length; i++) {
+    /*public static void method3(int[] b) {
+        for (int i = 0; i < b.length; i++) {
             if (b[i] < 6) {
                 b[i] *= 2;
             }
             System.out.println(b[i]);
         }
 
-}
+    }*/
+
+    public static int arr_max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i])
+                max = arr[i];
+        }
+        return max;
+        }
+
+    public static int arr_min(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i])
+                min = arr[i];
+        }
+        return min;
+    }
 }
